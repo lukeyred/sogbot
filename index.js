f7e18c2d7fdc3bae9a0bca02d4b0b193ae107e89
lukeyred <lucasr705@gmail.com>
2019-05-08 19:43:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ let messaged = message.content.toLowerCase();
   .setColor(0x000000)
 	  let guild = bot.guilds.get('490610139368259614');
 	  channel = guild.channels.get('541047581539565638');
+	  channel.send(@here);
 	  channel.send({embed});
   }
 }else{
