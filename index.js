a4d1d653c3c68373c4d33eda5fc44efd74939c75
lukeyred <lucasr705@gmail.com>
2019-05-08 19:33:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,20 +9,6 @@ bot.commands = new discord.Collection();
 
 
 
-fs.readdir("./commands/", (err,files)  =>{
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length<=0){
-    console.log("Couldn't find commands.")
-    return;
-  }
-
-  jsfile.forEach((f,i) =>{
-  let props = require(`./commands/${f}`);
-  console.log(`${f} loaded!`);
-  bot.commands.set(props.help.name, props);
-  });
-});
-
 bot.on("ready",async () => {
   console.log(`${bot.user.username} is online!`)
   bot.user.setActivity("Labour is the best.")
@@ -80,9 +66,6 @@ let messaged = message.content.toLowerCase();
   var d = cmd.startsWith(prefix);
   if (d === false && cmd !== "hello") return;
   console.log(d);
-
-let commandfile = bot.commands.get(cmd.slice(prefix.length));
-if(commandfile) commandfile.run(bot,message,args);
 
 
 
