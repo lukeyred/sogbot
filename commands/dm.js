31aab57cfeabcf53c84ae3438027e6a00d8a4c49
lukeyred <lucasr705@gmail.com>
2019-05-09 17:07:54 +0100

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -1,5 +1,7 @@
 const discord = require("discord.js");
 const superagent = require("superagent");
+
+
 module.exports.run = async (bot,message,args) =>{
 
 let tag = message.author.id;
@@ -15,16 +17,16 @@ const embed = new discord.RichEmbed()
   .setColor(0x000000)
 
 
-
+bot.fetchUser(`${args[0]}`).then((user) => {
+        user.send(`${embed}`);
+        message.channel.send("DM Sent");
+    });
 
 };
 
 
 
-bot.fetchUser(`${args[0]}`).then((user) => {
-        user.send(`${embed}`);
-        message.channel.send("DM Sent");
-    });
+
 
 
 
