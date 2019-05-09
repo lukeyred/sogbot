41c5e80979dfca0c543cc8901457269e0dd96633
lukeyred <lucasr705@gmail.com>
2019-05-09 16:53:38 +0100

    Update smstanding.js

--- commands/smstanding.js
+++ commands/smstanding.js
@@ -8,7 +8,7 @@ let dad = bot.guilds.get(message.guild.id).id;
   if (args[0] === "non-existent"){
     const embed = new discord.RichEmbed()
       .setTitle("Threat Level Updated")
-      .setDescription(`This case's threat level has been updated to: Non Existent.`)
+      .setDescription(`This case's threat level has been updated to: **Non Existent**.`)
       .setColor(0x40e57c)
       .setFooter(`By, ${message.author.username}`)
       .setTimestamp()
@@ -16,7 +16,7 @@ let dad = bot.guilds.get(message.guild.id).id;
   } else if (args[0] === "prevelant") {
     const embed = new discord.RichEmbed()
       .setTitle("Threat Level Updated")
-      .setDescription(`This case's threat level has been updated to: Prevelant.`)
+      .setDescription(`This case's threat level has been updated to: **Prevelant**.`)
       .setColor(0xedb53d)
       .setFooter(`By, ${message.author.username}`)
       .setTimestamp()
@@ -24,19 +24,25 @@ let dad = bot.guilds.get(message.guild.id).id;
   }else if (args[0] === "severe"){
     const embed = new discord.RichEmbed()
       .setTitle("Threat Level Updated")
-      .setDescription(`This case's threat level has been updated to: Severe`)
+      .setDescription(`This case's threat level has been updated to: **Severe**`)
       .setColor(0xef4a5a)
       .setFooter(`By, ${message.author.username}`)
       .setTimestamp()
     message.channel.send({embed});
+    let channel = message.channel;
+    let channelname = message.channel.name;
+    channel.setName(`${channelname}-🔴`)
   }else if (args[0] === "critical"){
         const embed = new discord.RichEmbed()
       .setTitle("Threat Level Updated")
-      .setDescription(`This case's threat level has been updated to: Critical`)
+      .setDescription(`This case's threat level has been updated to: **Critical**`)
       .setColor(0x000000)
       .setFooter(`By, ${message.author.username}`)
       .setTimestamp()
     message.channel.send({embed});
+          let channel = message.channel;
+    let channelname = message.channel.name;
+    channel.setName(`${channelname}-⚫`)
   }
     else{
     message.channel.send("THREAT LEVEL NOT RECOGNISED: **non-existent/prevelant/severe/critical.**");
